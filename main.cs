05e59779e4585d8fd2d6a6cb74c91dc67fb83164
ThadHodges <77890659+thadhodges@users.noreply.github.com>
2021-12-08 23:58:00 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) 
+  {
+    Console.WriteLine ("Hello World");
+  }
+}
+public class Jackson
+{
+   // Console.WriteLine($"Chicken scratch...")
+
+}
